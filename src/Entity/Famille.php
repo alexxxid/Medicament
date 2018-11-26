a1a27b8aab60249f4fbf10ff4a6f674a16241166
Lucas-Galbiati <galbiatilucas@outlook.fr>
2018-11-26 13:51:47 +0100

    relation

--- src/Entity/Famille.php
+++ src/Entity/Famille.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -21,6 +23,16 @@ class Famille
      */
     private $NomFamille;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Medicaments", mappedBy="famille")
+     */
+    private $medicaments;
+
+    public function __construct()
+    {
+        $this->medicaments = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -34,6 +46,37 @@ class Famille
     public function setNomFamille(string $NomFamille): self
     {
         $this->NomFamille = $NomFamille;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Medicaments[]
+     */
+    public function getMedicaments(): Collection
+    {
+        return $this->medicaments;
+    }
+
+    public function addMedicament(Medicaments $medicament): self
+    {
+        if (!$this->medicaments->contains($medicament)) {
+            $this->medicaments[] = $medicament;
+            $medicament->setFamille($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMedicament(Medicaments $medicament): self
+    {
+        if ($this->medicaments->contains($medicament)) {
+            $this->medicaments->removeElement($medicament);
+            // set the owning side to null (unless already changed)
+            if ($medicament->getFamille() === $this) {
+                $medicament->setFamille(null);
+            }
+        }
 
         return $this;
     }
