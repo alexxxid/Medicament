4235f757c4b35be12425606d3784a1f8bce3d7c2
alexxxid <alexis.lamour29@laposte.net>
2018-12-18 16:16:56 +0100

    ComposantS

---
+++ src/Form/ComposerType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Composer;
+use App\Entity\Composant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ComposerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+
+            ->add('composant', EntityType::class, array(
+                'class' => Composant::class,
+                'choice_label' => 'NomComposant',
+                'choice_attr' => [
+                    'class' => 'dropdown-content blue darken-3',
+                    'id' => 'ComposerFormComposantDd',
+
+                ],
+            ))
+            ->add('Quantite');
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Composer::class,
+        ]);
+    }
+}
